3a6b9c77f826f9766f8a43a9bb454f449cd6e3c2
TranTam <tamvaa1306@gmail.com>
2022-09-04 21:13:27 +0700

    component, global styles, props and notes

---
+++ vue-cli/more-about-vue-components/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <modal-comp
+    :modalTitle="modalTitle"
+    :modalContent="modalContent"
+    theme="contact"
+  ></modal-comp>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import ModalComp from "./components/ModalComp.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    ModalComp,
+  },
+  data() {
+    return {
+      modalTitle: "Modal title",
+      modalContent: "This is modal content",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/more-about-vue-components/src/components/ModalComp.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="modal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content" :class="{ 'bg-purple': theme == 'contact' }">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ modalTitle }}</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          >
+            X
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>{{ modalContent }}</p>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modalTitle: {
+      type: String,
+      default: "Title",
+      required: true,
+    },
+    modalContent: {
+      type: String,
+      default: "Title",
+      required: true,
+    },
+    theme: {
+      type: String,
+      validator(value) {
+        return ["example", "sales", "contact"].includes(value);
+      },
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1060;
+  /* display: none; */
+  display: block;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  outline: 0;
+}
+.fade {
+  transition: opacity 0.15s linear;
+}
+.modal.fade .modal-dialog {
+  transition: transform 0.3s ease-out;
+  transform: translate(0, -50px);
+}
+.modal-dialog {
+  max-width: 500px;
+  margin: 1.75rem auto;
+}
+.modal-content {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  pointer-events: auto;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 0.3rem;
+  outline: 0;
+}
+.modal-header {
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 1rem;
+  border-bottom: 1px solid #dee2e6;
+  border-top-left-radius: calc(0.3rem - 1px);
+  border-top-right-radius: calc(0.3rem - 1px);
+}
+.modal-body {
+  position: relative;
+  flex: 1 1 auto;
+  padding: 1rem;
+}
+.modal-footer {
+  display: flex;
+  flex-wrap: wrap;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0.75rem;
+  border-top: 1px solid #dee2e6;
+  border-bottom-right-radius: calc(0.3rem - 1px);
+  border-bottom-left-radius: calc(0.3rem - 1px);
+}
+.bg-purple {
+  background: rgb(67, 45, 67);
+  color: #fff;
+}
+</style>
