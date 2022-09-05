ab193f0eb195f74615702c89ef7802e6b5c17e6f
TranTam <tamvaa1306@gmail.com>
2022-09-05 09:05:15 +0700

    v-model: input and validate

--- vue-cli/data-binding-in-form-input/src/App.vue
+++ vue-cli/data-binding-in-form-input/src/App.vue
@@ -1,25 +1,14 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <quiz-form></quiz-form>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import QuizForm from "./components/QuizForm.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: { QuizForm },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
