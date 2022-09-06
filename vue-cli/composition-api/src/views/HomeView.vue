5ce0114ad74fe24c21afcd837d128f512cf2a597
TranTam <tamvaa1306@gmail.com>
2022-09-07 00:09:01 +0700

    ref vs reactive

--- vue-cli/composition-api/src/views/HomeView.vue
+++ vue-cli/composition-api/src/views/HomeView.vue
@@ -2,22 +2,31 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <p>{{ fullName }}</p>
+    <h2>{{ car.name }}</h2>
+    <h3>{{ car.price }}</h3>
     <button @click="onChange">Click me!</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 export default {
   name: "HomeView",
+  // Ref must have .value
+  // Ref is used for number, string...
+  // Reactive is used for object, array...
   setup() {
     const fullName = ref("Tran");
-
+    const car = reactive({
+      price: 1000,
+      name: "Mec",
+    });
     function onChange() {
       fullName.value = "Tran Tam";
+      car.price = 2000;
     }
-    return { fullName, onChange };
+    return { fullName, onChange, car };
   },
 };
 </script>
