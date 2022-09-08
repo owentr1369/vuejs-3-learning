9f34fc793978362e85361dec3bf13beec4bd32e2
TranTam <tamvaa1306@gmail.com>
2022-09-08 22:45:50 +0700

    pass data from store to component

--- vue-cli/vuex-4/src/views/HomeView.vue
+++ vue-cli/vuex-4/src/views/HomeView.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1 v-for="product in productList" :key="product.id">
+      {{ product.title }}
+    </h1>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "HomeView",
   setup() {},
   created() {
     this.$store.dispatch("fetchProducts");
   },
+  computed: {
+    ...mapGetters(["getProductList"]),
+    productList() {
+      return this.getProductList;
+    },
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  text-align: left;
+}
+</style>
