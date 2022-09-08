64968dac762f3c82e17e11d1fd300de2bee67e0e
TranTam <tamvaa1306@gmail.com>
2022-09-08 12:20:27 +0700

    fetch product try catch and v-if v-else-if v-else

--- vue-cli/fetching-data-composition-api/src/views/HomeView.vue
+++ vue-cli/fetching-data-composition-api/src/views/HomeView.vue
@@ -1,22 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div>
+    <div v-if="products.length" class="home">
+      <div class="product" v-for="product in products" :key="product.id">
+        <h1>{{ product.title }}</h1>
+        <img :src="product.image" alt="" />
+        <h3>Price: {{ product.price }}</h3>
+      </div>
+    </div>
+    <div v-else-if="error">{{ error }}</div>
+    <div v-else>Loading products...</div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "HomeView",
-  props: {
-    theme: {
-      type: String,
-      required: false,
-      default: "light",
-    },
-  },
-  setup(props, { emit }) {
-    console.log("props :>> ", props.theme);
-    console.log("emit :>> ", emit);
+  setup() {
+    const products = ref([]);
+    const error = ref(null);
+
+    const getProducts = async () => {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+
+        if (!res.ok) throw new Error("Something went wrong!");
+        products.value = await res.json();
+      } catch (err) {
+        error.value = err;
+        console.log(error.value);
+      }
+    };
+    getProducts();
+    return { products, error };
   },
 };
 </script>
+
+<style scoped>
+.home {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+.product {
+  width: 20%;
+}
+h1 {
+  font-size: 16px;
+}
+h3 {
+  font-size: 12px;
+}
+img {
+  width: 100%;
+}
+</style>
